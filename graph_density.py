285afd0f49f4ee4c5e7ccde0ffac888a2bc2fba8
g0r0kh <gorokhovivan09@gmail.com>
2024-08-20 15:00:08 +0300

    add groups to graph

--- graph_density.py
+++ graph_density.py
@@ -4,9 +4,20 @@ from base import data
 import numpy as np
 import pandas as pd
 #
+# >>> df = pd.DataFrame({
+# ...     'x': [1, 2, 2.5, 3, 3.5, 4, 5],
+# ...     'y': [4, 4, 4.5, 5, 5.5, 6, 6],
+# ... })
+# >>> ax = df.plot.kde()
+# data[data['year'] != 19]
+
+a = data[data['id#7'] == 'noExperience']
+b = data[data['id#7'] == 'between1And3']
+c = data[data['id#7'] == 'between3And6']
+d = data[data['id#7'] == 'moreThan6']
 
 
-s = pd.Series(data.RUR_net_000)
+s = pd.Series(a.RUR_net_000)
 ax = s.plot.kde()
 
 plt.grid()
