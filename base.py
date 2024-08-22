6284a533f491617108e02842c476f47bbcce5e5b
g0r0kh <gorokhovivan09@gmail.com>
2024-08-22 18:13:38 +0300

    add rename columns

--- base.py
+++ base.py
@@ -2,11 +2,15 @@ import pandas as pd
 import numpy as np
 
 # import xlsx
-data = pd.read_excel('source/source_HR.xlsx',
+data = pd.read_excel('source/source_HR_occup.xlsx',
                      sheet_name='sheet1')
 # add[000] dimension
 data['RUR_net_000'] = data['RUR_net'].astype(int)/1000
 
+
+
 # rename columns
-data = data.rename(columns={'id#7':'Experience'})
+data = data.rename(columns={'id#7':'Experience', 'name#5':'Occupation'})
+
+
 
